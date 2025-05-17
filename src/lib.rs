06f38a132c4473d90e955cac1f2d28f07876e4a7
haruki7049 <tontonkirikiri@gmail.com>
2025-05-17 17:50:28 +0900

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -27,15 +27,10 @@ pub enum Signal {
     Quit,
 }
 
-pub trait HrtorProcessor where Self: Sized {
+pub trait Processor {
     /// Handle the strings from inputs by main.rs on Hrtor implementation
     fn handle_command(&mut self, command: ReadResult) -> anyhow::Result<CommandStatus>;
 
     /// Evaluates the command
     fn eval(&mut self, str: String) -> anyhow::Result<CommandStatus>;
-}
-
-pub trait Hrtor {
-    /// Creates Hrtor instance by HrtorProcessor
-    fn new<P: HrtorProcessor>(processor: P) -> Self;
 }
